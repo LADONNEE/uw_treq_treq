48c5da6572c92eadfdc8fb92df2aeb6d9e54fc1b
Nadhir <nadhir@ladonnee.com>
2022-03-16 16:07:27 +0100

    Order types orser

--- resources/views/order-types/_menu-annotated.blade.php
+++ resources/views/order-types/_menu-annotated.blade.php
@@ -20,6 +20,25 @@
         </div>
     </div>
 
+    <?php $type = $types[1]; // (self::TRAVEL_REIMBURSEMENT, 'Travel Reimbursement', 'plane-arrival') ?>
+    <div>
+        <a class="order-menu__button" href="{{ route('project-create', $type->slug) }}">
+            <span>@icon($type->icon)</span> {{ $type->name }}
+        </a>
+        <div class="order-menu__help">
+            <p>Use after trip has occurred or to submit mileage reimbursements.</p>
+
+            <div class="order-menu__examples">Examples</div>
+            <ul>
+                <li>Mileage reimbursement</li>
+                <li>After travel for a pre-authorized trip</li>
+                <li>Travel missing pre-authorization (reimbursed as allowed)</li>
+                <li>Travel by staff, student and non-UW traveler</li>
+            </ul>
+        </div>
+    </div>
+
+
     <?php //$type = $types[1]; // ('pre-auth', 'Other Pre-Authorization', 'ballot-check') ?>
     <?php /* <div>
         <a class="order-menu__button" href="{{ route('project-create', $type->slug) }}">
@@ -42,6 +61,24 @@
     */
     ?>
 
+    <?php $type = $types[2]; // ('reimbursement', 'Other Reimbursement', 'receipt') ?>
+    <div>
+        <a class="order-menu__button" href="{{ route('project-create', $type->slug) }}">
+            <span>@icon($type->icon)</span> {{ $type->name }}
+        </a>
+        <div class="order-menu__help">
+            <p>Use for purchases already made with your own funds unrelated to travel.</p>
+
+            <div class="order-menu__examples">Examples</div>
+            <ul>
+                <li>Supplies</li>
+                <li>Membership</li>
+                <li>Subscriptions</li>
+                <li>Food</li>
+            </ul>
+        </div>
+    </div>
+
     <?php $type = $types[3]; // ('purchase', 'Make a Purchase', 'box-alt') ?>
     <div>
         <a class="order-menu__button" href="{{ route('project-create', $type->slug) }}">
@@ -60,41 +97,7 @@
         </div>
     </div>
 
-    <?php $type = $types[1]; // (self::TRAVEL_REIMBURSEMENT, 'Travel Reimbursement', 'plane-arrival') ?>
-    <div>
-        <a class="order-menu__button" href="{{ route('project-create', $type->slug) }}">
-            <span>@icon($type->icon)</span> {{ $type->name }}
-        </a>
-        <div class="order-menu__help">
-            <p>Use after trip has occurred or to submit mileage reimbursements.</p>
-
-            <div class="order-menu__examples">Examples</div>
-            <ul>
-                <li>Mileage reimbursement</li>
-                <li>After travel for a pre-authorized trip</li>
-                <li>Travel missing pre-authorization (reimbursed as allowed)</li>
-                <li>Travel by staff, student and non-UW traveler</li>
-            </ul>
-        </div>
-    </div>
-
-    <?php $type = $types[2]; // ('reimbursement', 'Other Reimbursement', 'receipt') ?>
-    <div>
-        <a class="order-menu__button" href="{{ route('project-create', $type->slug) }}">
-            <span>@icon($type->icon)</span> {{ $type->name }}
-        </a>
-        <div class="order-menu__help">
-            <p>Use for purchases already made with your own funds unrelated to travel.</p>
-
-            <div class="order-menu__examples">Examples</div>
-            <ul>
-                <li>Supplies</li>
-                <li>Membership</li>
-                <li>Subscriptions</li>
-                <li>Food</li>
-            </ul>
-        </div>
-    </div>
+    
 
     <?php $type = $types[4]; // ('invoice', 'Pay an Invoice', 'file-invoice') ?>
     <div>
