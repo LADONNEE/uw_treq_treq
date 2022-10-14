ade7b649ccdda53dae78819bdf58ac46fb34125b
Nadhir <nadhir@ladonnee.com>
2022-10-14 03:01:51 +0200

    aa0301

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -11,11 +11,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
           crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/css/app.css{{ $cacheBusting = '?v=' . config('view.resource_cache') }}" media="all" />
-    <link rel="stylesheet" type="text/css" href="/uworg-util/css/env-warning.css" type="text/css" media="all" />
-    <script defer src="/js/app.js{{ $cacheBusting }}"></script>
+    <link rel="stylesheet" type="text/css" href="/treq/css/app.css{{ $cacheBusting = '?v=' . config('view.resource_cache') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="/treq/uworg-util/css/env-warning.css" type="text/css" media="all" />
+    <script defer src="/treq/js/app.js{{ $cacheBusting }}"></script>
     <script defer src="https://kit.fontawesome.com/96343af987.js" crossorigin="anonymous"></script>
-    <script defer type="text/javascript" src="/uworg-util/js/env-warning.js"></script>
+    <script defer type="text/javascript" src="/treq/uworg-util/js/env-warning.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
 </head>
 <body>
--- resources/views/print/show.blade.php
+++ resources/views/print/show.blade.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
           crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/css/app.css{{ $cacheBusting = '?v=' . config('view.resource_cache') }}" media="all" />
+    <link rel="stylesheet" type="text/css" href="/treq/css/app.css{{ $cacheBusting = '?v=' . config('view.resource_cache') }}" media="all" />
     <script defer src="/js/app.js{{ $cacheBusting }}"></script>
     <script defer src="https://kit.fontawesome.com/96343af987.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
