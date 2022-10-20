373b5da587526fbe2c920ad7f17fdacbdc3bfb04
Nadhir <nadhir@ladonnee.com>
2022-10-20 21:26:28 +0200

    Fix api url

--- resources/js/notes/note-store.js
+++ resources/js/notes/note-store.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class NoteStore {
 
     constructor(id, section) {
-        this.apiRoot = '/api/notes/';
+        this.apiRoot = '/treq/api/notes/';
         this.id = id;
         this.section = section || '';
         this.notes = [];
