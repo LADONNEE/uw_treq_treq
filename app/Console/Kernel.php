5afa93e025038d8d1f8608e882f31da29feadef0
Nadhir <nadhir@ladonnee.com>
2022-12-09 15:31:27 +0100

    Kernel Command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('notify --all')->dailyAt('01:02');
+
     }
 
     /**
