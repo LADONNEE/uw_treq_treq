7c3c3a81c6d98a10b6993c24b2f5ec18282f9a83
Nadhir <nadhir@ladonnee.com>
2023-02-07 20:28:52 +0100

    Additional questions

--- app/Models/TripNotesCollection.php
+++ app/Models/TripNotesCollection.php
@@ -13,12 +13,22 @@ class TripNotesCollection
         'personal_time' => [
             'question' => 'Did any part of this request include personal time?',
             'label' => 'Personal Time',
-            'precision' => 'Please pro-rate Per Diem, Lodging, car rental & Long term Parking reimbursement (as applicable).'
+            'precision' => 'Please include dates, location and times. Please pro-rate Per Diem, Lodging, car rental & Long term Parking reimbursement (as applicable).'
         ],
         'airfare_needed' => [
             'question' => 'Is Comparision airfare needed for this reimbursment (If personal time is included with business travel)?',
             'label' => 'Airfare',
             'precision' => 'Please upload a copy of comparison airfare if required.'
+        ],
+        'flights_upgrades' => [
+            'question' => 'Were there any flight upgrades? (Pre-Approval Required)',
+            'label' => 'Upgraded Flight',
+            'precision' => 'Pre-Approval should be in TREQ. Please provide details including description & amounts when applicable.'
+        ],
+        'travel_conference' => [
+            'question' => 'Is this travel for a conference?',
+            'label' => 'Conference',
+            'precision' => 'Please look up conference dates (include a copy of the conference dates to Finance) to verify that travel dates comply with UW Policy'
         ],
         'travel_reimbursed' => [
             'question' => 'Was any part of this travel reimbursement previously reimbursed?',
@@ -31,15 +41,10 @@ class TripNotesCollection
             'label' => 'Sponsored',
             'precision' => 'Please provide details including description & amounts when applicable.'
         ],
-        'flights_upgrades' => [
-            'question' => 'Were there any flight upgrades?',
-            'label' => 'Upgraded Flight',
-            'precision' => 'Please provide details including description & amounts when applicable.'
-        ],
         'lodging_over' => [
             'question' => 'Did you pay more than maximum allowable lodging rate?',
             'label' => 'Over Lodging Per Diem',
-            'precision' => 'Please indicate amount and explain reason(s)',
+            'precision' => 'Please indicate amount and explain reason(s). Include a map if the hotel is more than 5 miles from the conference.',
             'options' => [
                 'Conference Hotel',
                 'Special Event or Disaster',
@@ -52,12 +57,17 @@ class TripNotesCollection
         'paid_for_other' => [
             'question' => 'Did you pay for any expenses for another traveler (lodging, airfare, registration, etc)?',
             'label' => 'Other Traveler',
-            'precision' => 'Please provide details of name(s) of traveler, description and amounts paid.'
+            'precision' => 'Please provide details of name(s) of traveler, description and amounts paid, including receipts'
         ],
         'meals_provided' => [
-            'question' => 'Were any meals provided? (List dates and meals provided.)',
+            'question' => 'For reimbursement of per diem, please confirm if  any meals were included?',
             'label' => 'Please list Meals and dates',
-            'precision' => 'Please provide details including description & amounts when applicable.'
+            'precision' => 'Please provide details of meals provided, including amounts and dates (example: breakfast, lunch, dinner)'
+        ],
+        'meals_banquet' => [
+            'question' => 'Was a  group banquet or reception part of this conference and was paid either in the registration or separately?',
+            'label' => 'Group banquet or reception',
+            'precision' => 'Please provide details, including description and amount when applicable.'
         ],
         'travel_changed' => [
             'question' => 'Did your travel plans change during trip (flight delays, came home early, etc.)?',
@@ -67,7 +77,7 @@ class TripNotesCollection
         'other_funding' => [
             'question' => 'Do you have other funding (not previously included on Pre-Travel Authorization)?',
             'label' => 'Other Funding',
-            'precision' => 'Please provide details including description & amounts when applicable.'
+            'precision' => 'Please provide details including Buget Approval, description & amounts when applicable.'
         ],
         'driving' => [
             'question' => ' Is this reimbursement for "driving" travel time?',
@@ -78,6 +88,11 @@ class TripNotesCollection
             'question' => 'Does any reimbursed amount differ from receipts enclosed in this reimbursement?',
             'label' => 'Reimbursed Amount',
             'precision' => 'If yes, please provide details and reason(s) for the difference.'
+        ],
+        'foreign_currency' => [
+            'question' => 'Are there charges in foreign currency?',
+            'label' => 'Foreign currency',
+            'precision' => 'Please use oanda.com for currency conversion & indicate the conversion amounts on the receipts.'
         ],
     ];
 
