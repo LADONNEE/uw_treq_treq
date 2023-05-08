533dfc6ec2c567f21cf5ca82552ee2a568dd2458
Nadhir <nadhir@ladonnee.com>
2023-05-08 15:11:13 +0200

    Add migration shared project codes

---
+++ database/migrations/2020_01_01_105749_create_shared_project_codes.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+class CreateSharedProjectCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement(sqlInclude(__DIR__ . '/../views/shared_view_project_codes.sql', [
+            '__DBSHARED__' => env('DB_DATABASE_SHARED', 'shared'),
+            '__DBBUDGETS__' => env('DB_DATABASE_BUDGETS', 'budgets')
+        ]));
+    }
+
+    public function down()
+    {
+        // there is no down
+    }
+}
---
+++ database/views/shared_view_project_codes.sql
@@ -0,0 +1,3 @@
+CREATE OR REPLACE VIEW __DBSHARED__.project_codes AS
+
+SELECT b.* from __DBBUDGETS__.`project_codes` `b` 
