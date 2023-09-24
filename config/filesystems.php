c1e8b278f59f819f8a8c914e484868df9b0a2c80
Nadhir <nadhir@ladonnee.com>
2023-09-24 15:07:59 +0200

    Larevel 9 upgrade

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,21 +13,9 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Cloud Filesystem Disk
-    |--------------------------------------------------------------------------
-    |
-    | Many applications store files both locally and in the cloud. For this
-    | reason, you may specify a default "cloud" driver here. This driver
-    | will be bound as the Cloud disk implementation in the container.
-    |
-    */
-
-    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
-
+    
     /*
     |--------------------------------------------------------------------------
     | Filesystem Disks
